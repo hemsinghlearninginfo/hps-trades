8ef06707607e50aefc75bfa7bae128c232f672d9
Hem Singh <hemsingh@publicisgroupe.net>
2018-09-26 00:44:20 +0530

    Start Registering

--- hps-trades-client/src/components/User/Register.js
+++ hps-trades-client/src/components/User/Register.js
@@ -4,27 +4,84 @@ import { NavLink } from 'reactstrap';
 
 import styles from './User.css';
 import Wrapper from '../../hoc/Wrapper';
-import Components from '../index';
 
 class Register extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {
+                firstName: '',
+                lastName: '',
+                email: '',
+                password: ''
+            },
+            submitted: false
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const { name, value } = event.target;
+        const { user } = this.state;
+        this.setState({
+            user: {
+                ...user,
+                [name]: value
+            }
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.setState({ submitted: true });
+        const { user } = this.state;
+        //const { dispatch } = this.props;
+        if (user.firstName && user.lastName && user.email && user.password) {
+            //dispatch(userActions.register(user));
+        }
+    }
+
     render() {
+        const { registering } = this.props;
+        const { user, submitted } = this.state;
         return (
             <Wrapper>
                 <div className="user-form">
-                    <form action="/examples/actions/confirmation.php" method="post">
+                    <form name="form" onSubmit={this.handleSubmit}>
                         <h2>Register</h2>
                         <p className="hint-text">Create your account. It's free and only takes a minute.</p>
                         <div className="form-group">
                             <div className="row">
-                                <div className="col-xs-6 firstName"><input type="text" className="form-control" name="first_name" placeholder="First Name" required="required" /></div>
-                                <div className="col-xs-6 lastName"><input type="text" className="form-control" name="last_name" placeholder="Last Name" required="required" /></div>
+                                <div className="col-xs-6 firstName">
+                                <input type="text" className="form-control" name="firstName" placeholder="First Name" required="required" value={user.firstName} onChange={this.handleChange} />
+                                    {
+                                        submitted && !user.firstName &&
+                                        <div className="help-block">First Name is required</div>
+                                    }
+                                </div>
+                                <div className="col-xs-6 lastName"><input type="text" className="form-control" name="lastName" placeholder="Last Name" required="required" value={user.lastName} onChange={this.handleChange} />
+                                    {
+                                        submitted && !user.lastName &&
+                                        <div className="help-block">Last Name is required</div>
+                                    }
+                                </div>
                             </div>
                         </div>
                         <div className="form-group">
-                            <input type="email" className="form-control" name="email" placeholder="Email" required="required" />
+                            <input type="email" className="form-control" name="email" placeholder="Email" required="required" value={user.email} onChange={this.handleChange} />
+                            {
+                                submitted && !user.email &&
+                                <div className="help-block">Email is required</div>
+                            }
                         </div>
                         <div className="form-group">
-                            <input type="password" className="form-control" name="password" placeholder="Password" required="required" />
+                            <input type="password" className="form-control" name="password" placeholder="Password" required="required" value={user.password} onChange={this.handleChange} />
+                            {
+                                submitted && !user.password &&
+                                <div className="help-block">Password is required</div>
+                            }
                         </div>
                         <div className="form-group">
                             <input type="password" className="form-control" name="confirm_password" placeholder="Confirm Password" required="required" />
