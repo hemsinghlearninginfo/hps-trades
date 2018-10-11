1ed17863cf498272b3b6eb40715ea81bf4636a1d
Hem Singh <hemsingh@publicisgroupe.net>
2018-10-11 22:38:43 +0530

    Start Adding Value in array

--- hps-trades-client/src/components/Event/events.js
+++ hps-trades-client/src/components/Event/events.js
@@ -10,6 +10,7 @@ import { getIcon } from '../../_helpers/';
 import '../../assets/css/react-datetime.css';
 
 
+
 class Events extends Component {
 
     constructor(props) {
@@ -17,50 +18,91 @@ class Events extends Component {
 
         this.state = {
             events: [],
-            types: []
+            types: [],
+            isValid: true,
+            date: moment().fromNow(),
+            newEvent: {
+                type: '',
+                heading: '',
+                message: '',
+            },
+            isAdd: false
         };
+
+        this.handleAddEventItem = this.handleAddEventItem.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        //this.handleDeleteItem = this.handleDeleteItem.bind(this);
+
+        //this.initialValue = this.initialValue.bind(this);
 
     }
 
     componentDidMount() {
-        let types = [];
-        types = ['warning', 'news', 'error', 'maintenance'];
-        this.setState({ types });
+        this.setState({ types: ['warning', 'news', 'error', 'maintenance'] });
+        //initialValue();
+    }
+
+    handleChange(event) {
+        const { name, value } = event.target;
+        const { newEvent } = this.state;
+        this.setState({
+            newEvent: {
+                ...newEvent,
+                [name]: value
+            }
+        });
+    }
+
+    handleAddEventItem() {
+        this.state.events.push(this.state.newEvent);
+        this.setState(
+            this.state
+        );
+        this.setState({
+            isAdd : !this.state.isAdd
+        })
+    }
+
+
+    initialValue = () => {
+        let initialEvents = {
+            date: moment().fromNow(),
+            type: '',
+            heading: '',
+            message: ''
+        }
+        this.setState({ events: [...this.state.events, initialEvents] });
     }
 
     addEmptyItem = () => {
-        let newEvent = {
-            isNew: true,
-            date: '10-20-2018',
-            type: 'error',
-            heading: 'heading',
-            message: 'this is my message'
-        }
-        let events = this.state.events;
-        events.push(newEvent);
         this.setState({
-            events
+            isAdd: !this.state.isAdd,
+            newEvent : {
+                heading:'',
+                message:''
+            }
         });
     }
 
     render() {
+        //initialValue();
+        const { events, types, isAdd, newEvent } = this.state;
 
-        const selectOptionsHTML = this.state.types.map((item) => {
+        const selectOptionsHTML = types.map((item) => {
             return (
                 <option key={item}>{item}</option>
             )
         });
 
-        const events = this.state.events;
         let eventItemsHTML = [];
         if (events.length > 0) {
-            eventItemsHTML = this.state.events.map((item, index) => {
+            eventItemsHTML = events.map((item, index) => {
                 if (!item.isNew) {
                     return (
                         <tr key={index}>
-                            <td>{item.date}</td>
-                            <td>{item.type}</td>
-                            <td>{item.heading}</td>
+                            <td className="date">{item.date}</td>
+                            <td className="type">{item.type}</td>
+                            <td className="heading">{item.heading}</td>
                             <td>{item.message}</td>
                             <td className="actions">
                                 <a href="#" className="editIcon" title="Edit">{getIcon(iconConstants.EDIT)}</a>
@@ -69,40 +111,42 @@ class Events extends Component {
                         </tr>
                     )
                 }
-                else {
-                    return (
-                        <tr key={index}>
-                            <td><Datetime /></td>
-                            <td><select>{selectOptionsHTML}</select></td>
-                            <td><input type="text" /></td>
-                            <td><input type="text" /></td>
-                            <td className="actions">
-                                <a href="#" className="saveIcon" title="Save">{getIcon(iconConstants.SAVE)}</a>
-                                <a href="#" className="cancelIcon" title="Cancel">{getIcon(iconConstants.CANCEL)}</a>
-                            </td>
-                        </tr>
-                    )
-                }
             })
         }
         else {
-            eventItemsHTML = (<tr><td colSpan="5" className="noDataFound">No event found</td></tr>)
+            eventItemsHTML = !isAdd && (<tr><td colSpan="5" className="noDataFound">No event found</td></tr>)
         }
+
+        let newItemHTML = isAdd && (
+            <tr>
+                <td className="date"><Datetime name="date" /></td>
+                <td className="type"><select name="type">{selectOptionsHTML}</select></td>
+                <td className="heading"><input name="heading" type="text" value={newEvent.heading} onChange={this.handleChange} /></td>
+                <td><input name="message" type="text" value={newEvent.message} onChange={this.handleChange} /></td>
+                <td className="actions">
+                    <a href="#" className="saveIcon" title="Save" onClick={this.handleAddEventItem}>{getIcon(iconConstants.SAVE)}</a>
+                    <a href="#" className="cancelIcon" title="Cancel">{getIcon(iconConstants.CANCEL)}</a>
+                </td>
+            </tr>
+        )
+
         return (
             <Components.PageTemplate iconType={iconConstants.Event} heading="Market Events">
+                {!this.state.isValid && <div className="errorBox">Error</div>}
                 <table className="table table-sm table-striped table-hover table-bordered">
                     <thead>
                         <tr>
-                            <th>
+                            <th className="date">
                                 <a href="#" className="addIcon" title="Add New Event" onClick={this.addEmptyItem} >{getIcon(iconConstants.ADD)}</a> Date
                                 </th>
-                            <th>Type</th>
-                            <th>Heading</th>
+                            <th className="type">Type</th>
+                            <th className="heading">Heading</th>
                             <th>Message</th>
-                            <th>Actions</th>
+                            <th className="actions">Actions</th>
                         </tr>
                     </thead>
                     <tbody>
+                        {newItemHTML}
                         {eventItemsHTML}
                     </tbody>
                 </table>
