57f88f02098179209c2ad3a1fbb1c6288ab61274
hem Singh <41054839+hemsinghlearninginfo@users.noreply.github.com>
2019-06-10 13:48:24 +0530

    handle Submit Method - UserStatus

--- hps-trades-client/src/components/User/UsersStatus.js
+++ hps-trades-client/src/components/User/UsersStatus.js
@@ -21,6 +21,8 @@ class UsersStatus extends Component {
             userRoles: [],
         }
         this.handleSearchChange = this.handleSearchChange.bind(this);
+        this.handleCheckBox = this.handleCheckBox.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount() {
@@ -55,15 +57,18 @@ class UsersStatus extends Component {
         }
     }
 
-    handleChange(event) {
-        // const { name, value } = event.target;
-        // const { newMapping } = this.state;
-        // this.setState({
-        //     newMapping: {
-        //         ...newMapping,
-        //         [name]: value
-        //     }
-        // });
+    handleCheckBox(id) {
+        const { users } = this.state;
+        let itemIndex = this.state.users.findIndex(item => item.id === id);
+        if (itemIndex !== -1) {
+            users[itemIndex].isRegistrationActive = !users[itemIndex].isRegistrationActive;
+            this.setState({ users });
+            this.handleSearchChange();
+        }
+    }
+
+    handleSubmit() {
+        const { users } = this.state;
     }
 
     render() {
@@ -77,49 +82,49 @@ class UsersStatus extends Component {
                     <td>
                         <div className="form-check">
                             <label className="form-check-label">
-                                <input type="checkbox" className="form-check-input" value="" defaultChecked={item.isRegistrationActive} />{item.isRegistrationActive ? 'Yes' : 'No'}
+                                <input type="checkbox" className="form-check-input" value="" defaultChecked={item.isRegistrationActive} onChange={e => this.handleCheckBox(item.id)} />{item.isRegistrationActive ? 'Yes' : 'No'}
                             </label>
                         </div>
                     </td>
-                        <td>{moment(item.createdDate).format('DD-MMM-YYYY, hh:mm a')}</td>
+                    <td>{moment(item.createdDate).format('DD-MMM-YYYY, hh:mm a')}</td>
                 </tr>
-                    );
-                });
+            );
+        });
 
-                return (
+        return (
             <Components.PageTemplate iconType={iconConstants.USERS_STATUS} heading="User Status">
-                        <div className="table-responsive">
-                            <div><input type="text" className="form-control form-control-sm" placeholder="Search data by username and name..." value={filterText} ref="filterTextInput" onChange={this.handleSearchChange.bind(this)} /></div>
-                            <table className="table table-hover bg-white border shadow table-bordered">
-                                <thead>
-                                    <tr className="font-weight-bold bg-info text-light">
-                                        <th>User Name</th>
-                                        <th>Name</th>
-                                        <th>Role</th>
-                                        <th>Is Active</th>
-                                        <th>Created Date</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {userHTMLTable}
-                                </tbody>
-                                <tfoot>
-                                    <tr><td colSpan="5"><button type="button" className="btn btn-sm btn-success">Success</button></td></tr>
-                                </tfoot>
-                            </table>
-                        </div>
-                    </Components.PageTemplate>
-                    );
-                }
-            }
+                <div className="table-responsive">
+                    <div><input type="text" className="form-control form-control-sm" placeholder="Search data by username and name..." value={filterText} ref="filterTextInput" onChange={this.handleSearchChange.bind(this)} /></div>
+                    <table className="table table-hover bg-white border shadow table-bordered">
+                        <thead>
+                            <tr className="font-weight-bold bg-info text-light">
+                                <th>User Name</th>
+                                <th>Name</th>
+                                <th>Role</th>
+                                <th>Is Active</th>
+                                <th>Created Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {userHTMLTable}
+                        </tbody>
+                        <tfoot>
+                            <tr><td colSpan="5"><button type="button" className="btn btn-sm btn-success" onClick={this.handleSubmit}>Update Records</button></td></tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </Components.PageTemplate>
+        );
+    }
+}
 
 function mapStateToProps(state) {
-    const {requestLoading} = state.generic;
-    const {authentication} = state;
+    const { requestLoading } = state.generic;
+    const { authentication } = state;
     return {
-                        requestLoading,
-                    authentication
-                };
-            }
-            
-            export default connect(mapStateToProps)(CSSModules(UsersStatus, styles)); 
+        requestLoading,
+        authentication
+    };
+}
+
+export default connect(mapStateToProps)(CSSModules(UsersStatus, styles)); 
